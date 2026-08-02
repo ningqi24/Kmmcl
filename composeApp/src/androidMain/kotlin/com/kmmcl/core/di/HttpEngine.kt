5847cf6ec7d00514ea9435bcb9ed56e5e242408e
ningqi <lyy201314_2023@qq.com>
2026-08-02 23:06:26 +0800

    remove: duplicate of existing file

--- composeApp/src/androidMain/kotlin/com/kmmcl/core/di/HttpEngine.kt
+++
@@ -1,7 +0,0 @@
-
-package com.kmmcl.core.di
-
-import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.android.Android
-
-internal fun httpClientEngine(): HttpClientEngine = Android.create()
