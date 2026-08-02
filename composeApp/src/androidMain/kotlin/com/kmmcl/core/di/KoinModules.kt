d0e471dabab56f22af7350eca2481f72a84d8510
ningqi <lyy201314_2023@qq.com>
2026-08-02 15:37:34 +0800

    fix: register GameViewModel in Koin module

--- composeApp/src/androidMain/kotlin/com/kmmcl/core/di/KoinModules.kt
+++ composeApp/src/androidMain/kotlin/com/kmmcl/core/di/KoinModules.kt
@@ -4,10 +4,12 @@ import com.kmmcl.core.auth.AuthService
 import com.kmmcl.core.download.DownloadManager
 import com.kmmcl.core.game.GameService
 import com.kmmcl.data.repository.GameRepository
+import com.kmmcl.ui.screens.game.GameViewModel
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -24,4 +26,7 @@ val appModule: Module = module {
     single { DownloadManager(get()) }
     single { GameService(get(), get()) }
     single { GameRepository(get()) }
+
+    // ViewModels
+    single { GameViewModel(get(), get()) }
 }
