80c337ef8eab36bb33dbec79718d66c1ba4edadc
ningqi <lyy201314_2023@qq.com>
2026-08-02 20:34:36 +0800

    fix: add VersionService, use proper gameDir from context

--- composeApp/src/androidMain/kotlin/com/kmmcl/core/di/KoinModules.kt
+++ composeApp/src/androidMain/kotlin/com/kmmcl/core/di/KoinModules.kt
@@ -1,19 +1,22 @@
+
 package com.kmmcl.core.di
 
 import com.kmmcl.core.auth.AuthService
 import com.kmmcl.core.download.DownloadManager
 import com.kmmcl.core.game.GameService
+import com.kmmcl.core.game.VersionService
 import com.kmmcl.data.repository.GameRepository
 import com.kmmcl.ui.screens.game.GameViewModel
-import io.ktor.client.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import java.io.File
 
 val appModule: Module = module {
     single<HttpClient> {
@@ -30,10 +33,17 @@ val appModule: Module = module {
     }
 
     single { AuthService(androidContext()) }
+    single { VersionService(get()) }
     single { DownloadManager(get()) }
-    single { GameService(get(), get()) }
     single { GameRepository(get()) }
+    single {
+        val ctx = androidContext()
+        GameService(
+            versionService = get(),
+            downloadManager = get(),
+            gameDir = File(ctx.getExternalFilesDir(null), "game")
+        )
+    }
 
-    // ViewModels
     single { GameViewModel(androidApplication(), get(), get()) }
 }
