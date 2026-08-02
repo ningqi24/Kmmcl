2c042102dc492a6971b444f073b4b8ba6a56d010
ningqi <lyy201314_2023@qq.com>
2026-08-02 13:34:25 +0800

    cleanup: remove misplaced files

--- src/androidMain/kotlin/com/kmmcl/core/di/Platform.kt
+++
@@ -1,6 +0,0 @@
-package com.kmmcl.core.di
-
-import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.android.Android
-
-fun httpClientEngine(): HttpClientEngine = Android.create()
