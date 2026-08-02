1a2e89911a1189a976d5faf340bf1625fec7b14a
ningqi <lyy201314_2023@qq.com>
2026-08-02 12:45:10 +0800

    refactor: rewrite core services with Ktor + stdlib, remove mokt/kdownloadfiles/kzip

--- composeApp/src/main/kotlin/com/kmmcl/core/di/KoinModules.kt
+++ composeApp/src/main/kotlin/com/kmmcl/core/di/KoinModules.kt
@@ -3,24 +3,25 @@ package com.kmmcl.core.di
 import com.kmmcl.core.auth.AuthService
 import com.kmmcl.core.download.DownloadManager
 import com.kmmcl.core.game.GameService
-import com.kmmcl.ui.screens.game.GameViewModel
-import org.koin.core.context.startKoin
+import com.kmmcl.data.repository.GameRepository
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
-val coreModule = module {
-    single { AuthService() }
-    single { DownloadManager() }
-    single { GameService() }
-}
-
-val viewModelModule = module {
-    single { GameViewModel(get(), get(), get()) }
-}
-
-fun initKoin(platformModules: org.koin.core.module.Module.() -> Unit = {}) {
-    startKoin {
-        modules(platformModules)
-        modules(coreModule)
-        modules(viewModelModule)
+val appModule: Module = module {
+    single<HttpClient> {
+        HttpClient(httpClientEngine()) {
+            install(ContentNegotiation) {
+                json(Json { ignoreUnknownKeys = true })
+            }
+        }
     }
+
+    single { AuthService() }
+    single { DownloadManager(get()) }
+    single { GameService(get(), get()) }
+    single { GameRepository(get()) }
 }
