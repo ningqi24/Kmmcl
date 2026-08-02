26babb905b51e38f392f8c50646861b60032d054
ningqi <lyy201314_2023@qq.com>
2026-08-02 13:18:21 +0800

    fix: migrate to androidMain sourceSet and resolve compile errors

---
+++ src/androidMain/kotlin/com/kmmcl/App.kt
@@ -0,0 +1,11 @@
+package com.kmmcl
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun AppPreview() {
+    Text("Kmmcl Preview")
+}
