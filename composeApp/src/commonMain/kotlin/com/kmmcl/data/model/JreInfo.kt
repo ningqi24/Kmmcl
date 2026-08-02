9af66fff52d6c8866ee3c40925af8bff89edf5e1
ningqi <lyy201314_2023@qq.com>
2026-08-02 22:03:32 +0800

    fix: JRE_21->JRE_17, correct PojavLauncher URLs

--- composeApp/src/commonMain/kotlin/com/kmmcl/data/model/JreInfo.kt
+++ composeApp/src/commonMain/kotlin/com/kmmcl/data/model/JreInfo.kt
@@ -7,15 +7,17 @@ import kotlinx.serialization.Serializable
 data class JreInfo(
     val version: String,
     val arm64Url: String,
-    val armeabiUrl: String,
+    val armUrl: String,
+    val x86Url: String,
     val x8664Url: String
 ) {
     companion object {
-        val JRE_21 = JreInfo(
-            version = "21",
-            arm64Url  = "https://github.com/PojavLauncherTeam/android-openjdk-build-multiarch/releases/download/jre21-rollback-20240505/jre21-arm64-v8a-release.tar.xz",
-            armeabiUrl = "https://github.com/PojavLauncherTeam/android-openjdk-build-multiarch/releases/download/jre21-rollback-20240505/jre21-armeabi-v7a-release.tar.xz",
-            x8664Url  = "https://github.com/PojavLauncherTeam/android-openjdk-build-multiarch/releases/download/jre21-rollback-20240505/jre21-x86_64-release.tar.xz",
+        val JRE_17 = JreInfo(
+            version = "17",
+            arm64Url  = "https://github.com/PojavLauncherTeam/android-openjdk-build-multiarch/releases/download/jre17-ec28559/jre17-arm64-20210825-release.tar.xz",
+            armUrl    = "https://github.com/PojavLauncherTeam/android-openjdk-build-multiarch/releases/download/jre17-ec28559/jre17-arm-20210914-release.tar.xz",
+            x86Url    = "https://github.com/PojavLauncherTeam/android-openjdk-build-multiarch/releases/download/jre17-ec28559/jre17-x86-20220225-release.tar.xz",
+            x8664Url  = "https://github.com/PojavLauncherTeam/android-openjdk-build-multiarch/releases/download/jre17-ec28559/jre17-x86_64-20210825-release.tar.xz",
         )
     }
 }
